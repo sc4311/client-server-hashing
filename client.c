e56f50a64697f695e2453d7a8b5a894ee61ca9cf
sc4311 <006lions@gmail.com>
2024-08-06 16:35:48 +0100

    adding comments

--- client.c
+++ client.c
@@ -1,138 +1,138 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <time.h>
-#include "sha256_lib.h"
+#include <stdio.h> // Standard I/O library
+#include <stdlib.h> // Standard library for memory allocation, process control, etc.
+#include <string.h> // String handling functions
+#include <unistd.h> // POSIX API for Unix-like systems
+#include <arpa/inet.h> // Definitions for internet operations
+#include <time.h> // Time-related functions
+#include "sha256_lib.h" // Custom SHA-256 library
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 1024 // Buffer size for reading data
 
-void handle_connection(int sock);
-void sha256(const char *input, size_t len, unsigned char output[SHA256_DIGEST_SIZE]);
+void handle_connection(int sock); // Function to handle connection with the server
+void sha256(const char *input, size_t len, unsigned char output[SHA256_DIGEST_SIZE]); // Function to compute SHA-256 hash
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
+    if (argc != 3) { // Check if the correct number of arguments is provided
         fprintf(stderr, "Usage: %s <hostname> <port>\n", argv[0]);
-        exit(EXIT_FAILURE);
+        exit(EXIT_FAILURE); // Exit if arguments are incorrect
     }
 
-    char *hostname = argv[1];
-    int port = atoi(argv[2]);
-    
-    int sock;
-    struct sockaddr_in server_addr;
-    
+    char *hostname = argv[1]; // Get the hostname from arguments
+    int port = atoi(argv[2]); // Convert port argument to integer
+
+    int sock; // Socket descriptor
+    struct sockaddr_in server_addr; // Structure to hold server address
+
     // Create socket
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Socket creation error");
-        exit(EXIT_FAILURE);
-    }
-    
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
-    
-    // Convert hostname to IP address
-    if (inet_pton(AF_INET, hostname, &server_addr.sin_addr) <= 0) {
-        perror("Invalid address/ Address not supported");
-        exit(EXIT_FAILURE);
-    }
-    
-    // Connect to server
-    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Connection failed");
-        exit(EXIT_FAILURE);
+        perror("Socket creation error"); // Print error if socket creation fails
+        exit(EXIT_FAILURE); // Exit if socket creation fails
     }
 
-    handle_connection(sock);
-    close(sock);
-    return 0;
+    server_addr.sin_family = AF_INET; // Set address family to Internet
+    server_addr.sin_port = htons(port); // Set port number
+
+    // Convert hostname to IP address
+    if (inet_pton(AF_INET, hostname, &server_addr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported"); // Print error if address conversion fails
+        exit(EXIT_FAILURE); // Exit if address conversion fails
+    }
+
+    // Connect to server
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connection failed"); // Print error if connection fails
+        exit(EXIT_FAILURE); // Exit if connection fails
+    }
+
+    handle_connection(sock); // Handle the connection with the server
+    close(sock); // Close the socket
+    return 0; // Return 0 to indicate successful execution
 }
 
 void handle_connection(int sock) {
-    char buffer[BUFFER_SIZE];
-    int option;
-    char input[BUFFER_SIZE];
-    unsigned char hash[SHA256_DIGEST_SIZE];
-    char hash_str[SHA256_DIGEST_SIZE * 2 + 1];
-    clock_t start, end;
-    double cpu_time_used;
+    char buffer[BUFFER_SIZE]; // Buffer to hold data from the server
+    int option; // Variable to store user option
+    char input[BUFFER_SIZE]; // Buffer to hold user input
+    unsigned char hash[SHA256_DIGEST_SIZE]; // Buffer to hold SHA-256 hash
+    char hash_str[SHA256_DIGEST_SIZE * 2 + 1]; // Buffer to hold hash as a string
+    clock_t start, end; // Variables to measure time
+    double cpu_time_used; // Variable to store CPU time used
 
     while (1) {
         printf("Enter option (1: check username/email, 2: check password, 3: check both, 4: exit): ");
-        scanf("%d", &option);
-        getchar(); // consume newline
+        scanf("%d", &option); // Get user option
+        getchar(); // Consume newline
 
-        if (option == 4) {
-            send(sock, "exit", strlen("exit"), 0);
-            break;
+        if (option == 4) { // If user wants to exit
+            send(sock, "exit", strlen("exit"), 0); // Send exit command to server
+            break; // Exit the loop
         }
 
-        if (option == 1) {
+        if (option == 1) { // If user wants to check username/email
             printf("Enter username/email: ");
-            fgets(input, BUFFER_SIZE, stdin);
-            input[strcspn(input, "\n")] = 0; // remove newline
-            sha256(input, strlen(input), hash);
+            fgets(input, BUFFER_SIZE, stdin); // Get username/email from user
+            input[strcspn(input, "\n")] = 0; // Remove newline
+            sha256(input, strlen(input), hash); // Compute SHA-256 hash
             for (int i = 0; i < SHA256_DIGEST_SIZE; i++) {
-                sprintf(hash_str + (i * 2), "%02x", hash[i]);
+                sprintf(hash_str + (i * 2), "%02x", hash[i]); // Convert hash to string
             }
             printf("Username/email hash: %s\n", hash_str); // Debug print
-            sprintf(buffer, "check_username:%s", hash_str);
-            send(sock, buffer, strlen(buffer), 0);
+            sprintf(buffer, "check_username:%s", hash_str); // Prepare buffer to send to server
+            send(sock, buffer, strlen(buffer), 0); // Send buffer to server
         }
 
-        if (option == 2) {
+        if (option == 2) { // If user wants to check password
             printf("Enter password: ");
-            fgets(input, BUFFER_SIZE, stdin);
-            input[strcspn(input, "\n")] = 0; // remove newline
-            sha256(input, strlen(input), hash);
+            fgets(input, BUFFER_SIZE, stdin); // Get password from user
+            input[strcspn(input, "\n")] = 0; // Remove newline
+            sha256(input, strlen(input), hash); // Compute SHA-256 hash
             for (int i = 0; i < SHA256_DIGEST_SIZE; i++) {
-                sprintf(hash_str + (i * 2), "%02x", hash[i]);
+                sprintf(hash_str + (i * 2), "%02x", hash[i]); // Convert hash to string
             }
             printf("Password hash: %s\n", hash_str); // Debug print
-            sprintf(buffer, "check_password:%s", hash_str);
-            send(sock, buffer, strlen(buffer), 0);
+            sprintf(buffer, "check_password:%s", hash_str); // Prepare buffer to send to server
+            send(sock, buffer, strlen(buffer), 0); // Send buffer to server
         }
 
-        if (option == 3) {
-            char username_hash[SHA256_DIGEST_SIZE * 2 + 1];
-            char password_hash[SHA256_DIGEST_SIZE * 2 + 1];
+        if (option == 3) { // If user wants to check both username/email and password
+            char username_hash[SHA256_DIGEST_SIZE * 2 + 1]; // Buffer to hold username hash
+            char password_hash[SHA256_DIGEST_SIZE * 2 + 1]; // Buffer to hold password hash
 
             printf("Enter username/email: ");
-            fgets(input, BUFFER_SIZE, stdin);
-            input[strcspn(input, "\n")] = 0; // remove newline
-            sha256(input, strlen(input), hash);
+            fgets(input, BUFFER_SIZE, stdin); // Get username/email from user
+            input[strcspn(input, "\n")] = 0; // Remove newline
+            sha256(input, strlen(input), hash); // Compute SHA-256 hash
             for (int i = 0; i < SHA256_DIGEST_SIZE; i++) {
-                sprintf(username_hash + (i * 2), "%02x", hash[i]);
+                sprintf(username_hash + (i * 2), "%02x", hash[i]); // Convert hash to string
             }
             printf("Username/email hash: %s\n", username_hash); // Debug print
 
             printf("Enter password: ");
-            fgets(input, BUFFER_SIZE, stdin);
-            input[strcspn(input, "\n")] = 0; // remove newline
-            sha256(input, strlen(input), hash);
+            fgets(input, BUFFER_SIZE, stdin); // Get password from user
+            input[strcspn(input, "\n")] = 0; // Remove newline
+            sha256(input, strlen(input), hash); // Compute SHA-256 hash
             for (int i = 0; i < SHA256_DIGEST_SIZE; i++) {
-                sprintf(password_hash + (i * 2), "%02x", hash[i]);
+                sprintf(password_hash + (i * 2), "%02x", hash[i]); // Convert hash to string
             }
             printf("Password hash: %s\n", password_hash); // Debug print
 
-            sprintf(buffer, "check_both:%s:%s", username_hash, password_hash);
-            send(sock, buffer, strlen(buffer), 0);
+            sprintf(buffer, "check_both:%s:%s", username_hash, password_hash); // Prepare buffer to send to server
+            send(sock, buffer, strlen(buffer), 0); // Send buffer to server
         }
 
-        start = clock();
-        int valread = read(sock, buffer, BUFFER_SIZE);
-        end = clock();
-        buffer[valread] = '\0';
-        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-        printf("Server response: %s\n", buffer);
-        printf("Response time: %f seconds\n", cpu_time_used);
+        start = clock(); // Start time measurement
+        int valread = read(sock, buffer, BUFFER_SIZE); // Read response from server
+        end = clock(); // End time measurement
+        buffer[valread] = '\0'; // Null-terminate the buffer
+        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC; // Calculate CPU time used
+        printf("Server response: %s\n", buffer); // Print server response
+        printf("Response time: %f seconds\n", cpu_time_used); // Print response time
     }
 }
 
 void sha256(const char *input, size_t len, unsigned char output[SHA256_DIGEST_SIZE]) {
-    SHA256_CTX ctx;
-    sha256_init(&ctx);
-    sha256_update(&ctx, (const uint8_t *)input, len);
-    sha256_final(&ctx, output);
+    SHA256_CTX ctx; // SHA-256 context
+    sha256_init(&ctx); // Initialize SHA-256 context
+    sha256_update(&ctx, (const uint8_t *)input, len); // Update SHA-256 context with input
+    sha256_final(&ctx, output); // Finalize SHA-256 hash
 }
--- server.c
+++ server.c
@@ -1,195 +1,195 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <signal.h>
-#include "sha256_lib.h"
+#include <stdio.h> // Standard I/O library
+#include <stdlib.h> // Standard library for memory allocation, process control, etc.
+#include <string.h> // String handling functions
+#include <unistd.h> // POSIX API for Unix-like systems
+#include <arpa/inet.h> // Definitions for internet operations
+#include <signal.h> // Signal handling
+#include "sha256_lib.h" // Custom SHA-256 library
 
-#define BUFFER_SIZE 1024
-#define MAX_CREDENTIALS 1000000
+#define BUFFER_SIZE 1024 // Buffer size for reading data
+#define MAX_CREDENTIALS 1000000 // Maximum number of credentials
 
-char credentials[MAX_CREDENTIALS][SHA256_DIGEST_SIZE * 2 + 1];
-int credential_count = 0;
-int server_fd;
+char credentials[MAX_CREDENTIALS][SHA256_DIGEST_SIZE * 2 + 1]; // Array to store credentials
+int credential_count = 0; // Counter for the number of credentials
+int server_fd; // Server file descriptor
 
-void load_credentials(const char *filename);
-void handle_client(int client_sock);
-void sigint_handler(int sig);
+void load_credentials(const char *filename); // Function to load credentials from a file
+void handle_client(int client_sock); // Function to handle client connections
+void sigint_handler(int sig); // Signal handler for SIGINT
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
+    if (argc != 3) { // Check if the correct number of arguments is provided
         fprintf(stderr, "Usage: %s <port> <credentials_file>\n", argv[0]);
-        exit(EXIT_FAILURE);
+        exit(EXIT_FAILURE); // Exit if arguments are incorrect
     }
 
-    int port = atoi(argv[1]);
-    const char *credentials_file = argv[2];
+    int port = atoi(argv[1]); // Convert port argument to integer
+    const char *credentials_file = argv[2]; // Get the credentials file path
 
-    load_credentials(credentials_file);
+    load_credentials(credentials_file); // Load credentials from the file
 
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
+    struct sockaddr_in address; // Structure to hold server address
+    int addrlen = sizeof(address); // Length of the address structure
 
     // Create socket
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket failed");
-        exit(EXIT_FAILURE);
+        perror("Socket failed"); // Print error if socket creation fails
+        exit(EXIT_FAILURE); // Exit if socket creation fails
     }
 
     // Set socket options to reuse port
     int opt = 1;
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
+        perror("setsockopt"); // Print error if setting socket options fails
+        exit(EXIT_FAILURE); // Exit if setting socket options fails
     }
 
     // Bind to port
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(port);
+    address.sin_family = AF_INET; // Set address family to Internet
+    address.sin_addr.s_addr = INADDR_ANY; // Accept connections from any IP address
+    address.sin_port = htons(port); // Set port number
 
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Bind failed");
-        exit(EXIT_FAILURE);
+        perror("Bind failed"); // Print error if binding fails
+        exit(EXIT_FAILURE); // Exit if binding fails
     }
 
     // Listen for connections
     if (listen(server_fd, 3) < 0) {
-        perror("Listen failed");
-        exit(EXIT_FAILURE);
+        perror("Listen failed"); // Print error if listening fails
+        exit(EXIT_FAILURE); // Exit if listening fails
     }
 
-    signal(SIGINT, sigint_handler);
+    signal(SIGINT, sigint_handler); // Set up signal handler for SIGINT
 
-    printf("Server listening on port %d\n", port);
+    printf("Server listening on port %d\n", port); // Print server listening message
 
-    while (1) {
+    while (1) { // Infinite loop to accept and handle client connections
         int client_sock;
         if ((client_sock = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-            perror("Accept failed");
-            exit(EXIT_FAILURE);
+            perror("Accept failed"); // Print error if accepting connection fails
+            exit(EXIT_FAILURE); // Exit if accepting connection fails
         }
 
-        handle_client(client_sock);
-        close(client_sock);
+        handle_client(client_sock); // Handle the client connection
+        close(client_sock); // Close the client socket
     }
 
-    return 0;
+    return 0; // Return 0 to indicate successful execution
 }
 
 void load_credentials(const char *filename) {
-    FILE *file = fopen(filename, "r");
+    FILE *file = fopen(filename, "r"); // Open the credentials file for reading
     if (!file) {
-        perror("Failed to open credentials file");
-        exit(EXIT_FAILURE);
+        perror("Failed to open credentials file"); // Print error if file opening fails
+        exit(EXIT_FAILURE); // Exit if file opening fails
     }
 
-    char line[BUFFER_SIZE];
+    char line[BUFFER_SIZE]; // Buffer to hold each line from the file
     while (fgets(line, sizeof(line), file) && credential_count < MAX_CREDENTIALS) {
-        line[strcspn(line, "\n")] = 0; // remove newline
+        line[strcspn(line, "\n")] = 0; // Remove newline character from the line
 
         // Split the line by ':' and store each part as a separate credential
         char *token = strtok(line, ":");
         while (token != NULL && credential_count < MAX_CREDENTIALS) {
-            strcpy(credentials[credential_count++], token);
-            token = strtok(NULL, ":");
+            strcpy(credentials[credential_count++], token); // Copy token to credentials array
+            token = strtok(NULL, ":"); // Get the next token
         }
     }
 
-    fclose(file);
+    fclose(file); // Close the file
 }
 
 void handle_client(int client_sock) {
-    char buffer[BUFFER_SIZE];
-    int valread;
-    int check_username = 0;
-    int check_password = 0;
-    int check_both = 0;
-    char username_hash[SHA256_DIGEST_SIZE * 2 + 1] = {0};
-    char password_hash[SHA256_DIGEST_SIZE * 2 + 1] = {0};
+    char buffer[BUFFER_SIZE]; // Buffer to hold data from the client
+    int valread; // Number of bytes read from the client
+    int check_username = 0; // Flag to check username
+    int check_password = 0; // Flag to check password
+    int check_both = 0; // Flag to check both username and password
+    char username_hash[SHA256_DIGEST_SIZE * 2 + 1] = {0}; // Buffer to hold username hash
+    char password_hash[SHA256_DIGEST_SIZE * 2 + 1] = {0}; // Buffer to hold password hash
 
-    while ((valread = read(client_sock, buffer, BUFFER_SIZE)) > 0) {
-        buffer[valread] = '\0';
+    while ((valread = read(client_sock, buffer, BUFFER_SIZE)) > 0) { // Read data from the client
+        buffer[valread] = '\0'; // Null-terminate the buffer
 
-        if (strncmp(buffer, "exit", 4) == 0) {
-            break;
+        if (strncmp(buffer, "exit", 4) == 0) { // Check if the client wants to exit
+            break; // Exit the loop
         }
 
         // Determine the type of check
         if (strncmp(buffer, "check_username:", 15) == 0) {
-            check_username = 1;
-            strcpy(username_hash, buffer + 15);
+            check_username = 1; // Set flag to check username
+            strcpy(username_hash, buffer + 15); // Copy username hash from buffer
             printf("Received username hash: %s\n", username_hash); // Debug print
         } else if (strncmp(buffer, "check_password:", 15) == 0) {
-            check_password = 1;
-            strcpy(password_hash, buffer + 15);
+            check_password = 1; // Set flag to check password
+            strcpy(password_hash, buffer + 15); // Copy password hash from buffer
             printf("Received password hash: %s\n", password_hash); // Debug print
         } else if (strncmp(buffer, "check_both:", 11) == 0) {
-            check_both = 1;
-            char *token = strtok(buffer + 11, ":");
+            check_both = 1; // Set flag to check both username and password
+            char *token = strtok(buffer + 11, ":"); // Split buffer to get username hash
             if (token) {
-                strcpy(username_hash, token);
-                token = strtok(NULL, ":");
+                strcpy(username_hash, token); // Copy username hash
+                token = strtok(NULL, ":"); // Get the next token for password hash
                 if (token) {
-                    strcpy(password_hash, token);
+                    strcpy(password_hash, token); // Copy password hash
                 }
             }
             printf("Received both hashes - Username: %s, Password: %s\n", username_hash, password_hash); // Debug print
         }
 
-        int found_username = 0, found_password = 0;
-        if (check_username || check_both) {
+        int found_username = 0, found_password = 0; // Flags to indicate if username/password is found
+        if (check_username || check_both) { // Check if username or both need to be checked
             for (int i = 0; i < credential_count; i++) {
                 printf("Comparing username hash with credential: %s\n", credentials[i]); // Debug print
-                if (strcmp(username_hash, credentials[i]) == 0) {
-                    found_username = 1;
-                    break;
+                if (strcmp(username_hash, credentials[i]) == 0) { // Compare username hash
+                    found_username = 1; // Set flag if username is found
+                    break; // Exit the loop
                 }
             }
         }
 
-        if (check_password || check_both) {
+        if (check_password || check_both) { // Check if password or both need to be checked
             for (int i = 0; i < credential_count; i++) {
                 printf("Comparing password hash with credential: %s\n", credentials[i]); // Debug print
-                if (strcmp(password_hash, credentials[i]) == 0) {
-                    found_password = 1;
-                    break;
+                if (strcmp(password_hash, credentials[i]) == 0) { // Compare password hash
+                    found_password = 1; // Set flag if password is found
+                    break; // Exit the loop
                 }
             }
         }
 
-        if (check_both) {
+        if (check_both) { // If both username and password need to be checked
             if (found_username && found_password) {
-                send(client_sock, "FoundBoth", strlen("FoundBoth"), 0);
+                send(client_sock, "FoundBoth", strlen("FoundBoth"), 0); // Send response if both are found
             } else if (found_username) {
-                send(client_sock, "FoundUsernameOnly", strlen("FoundUsernameOnly"), 0);
+                send(client_sock, "FoundUsernameOnly", strlen("FoundUsernameOnly"), 0); // Send response if only username is found
             } else if (found_password) {
-                send(client_sock, "FoundPasswordOnly", strlen("FoundPasswordOnly"), 0);
+                send(client_sock, "FoundPasswordOnly", strlen("FoundPasswordOnly"), 0); // Send response if only password is found
             } else {
-                send(client_sock, "NotFound", strlen("NotFound"), 0);
+                send(client_sock, "NotFound", strlen("NotFound"), 0); // Send response if neither is found
             }
-        } else if (check_username) {
+        } else if (check_username) { // If only username needs to be checked
             if (found_username) {
-                send(client_sock, "Found", strlen("Found"), 0);
+                send(client_sock, "Found", strlen("Found"), 0); // Send response if username is found
             } else {
-                send(client_sock, "Not Found", strlen("Not Found"), 0);
+                send(client_sock, "Not Found", strlen("Not Found"), 0); // Send response if username is not found
             }
-        } else if (check_password) {
+        } else if (check_password) { // If only password needs to be checked
             if (found_password) {
-                send(client_sock, "Found", strlen("Found"), 0);
+                send(client_sock, "Found", strlen("Found"), 0); // Send response if password is found
             } else {
-                send(client_sock, "Not Found", strlen("Not Found"), 0);
+                send(client_sock, "Not Found", strlen("Not Found"), 0); // Send response if password is not found
             }
         }
 
         // Reset check flags
-        check_username = check_password = check_both = 0;
+        check_username = check_password = check_both = 0; // Reset flags for the next iteration
     }
 }
 
 void sigint_handler(int sig) {
-    printf("Caught signal %d, closing server...\n", sig);
-    close(server_fd);
-    exit(0);
+    printf("Caught signal %d, closing server...\n", sig); // Print signal caught message
+    close(server_fd); // Close the server socket
+    exit(0); // Exit the program
 }
